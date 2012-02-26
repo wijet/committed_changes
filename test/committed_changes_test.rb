991b991a702e532cf9b04bcc69deb4b0fb7ef16f
Mariusz Pietrzyk <wijet@wijet.pl>
2012-02-26 18:00:06 +0100

    Added Model#attribute_changed_and_committed?

--- test/committed_changes_test.rb
+++ test/committed_changes_test.rb
@@ -5,18 +5,31 @@ class CommittedChangesTest < ActiveSupport::TestCase
   self.use_transactional_fixtures = false
 
   fixtures :vhosts
-  
-  test "storing committed changes in committed_changes attribute" do
+
+  test "#committed_changes returns changes hash after commit" do
     foo = Vhost.find_by_name("Foo")
     foo.domains = "bar.example.com"
     foo.save
     assert_equal %w[foo.example.com bar.example.com], foo.committed_changes[:domains]
+    assert_nil foo.committed_changes[:name]
   end
-  
-  test "committed_changes returns empty hash when save fails" do
+
+  test "#committed_changes returns empty hash when transaction is not comitted" do
     foo = Vhost.find_by_name("Foo")
+    # domains must be present
     foo.domains = nil
     foo.save
     assert_equal({}, foo.committed_changes)
+  end
+
+  test '#{attribute}_changed_and_committed?' do
+    foo = Vhost.find_by_name("Foo")
+    foo.name = "bar"
+    foo.save
+    assert !foo.domains_changed_and_committed?
+
+    foo.domains = "bar.example.com"
+    foo.save
+    assert foo.domains_changed_and_committed?
   end
 end
--- test/dummy/app/models/vhost.rb
+++ test/dummy/app/models/vhost.rb
@@ -1,5 +1,4 @@
 class Vhost < ActiveRecord::Base
   include CommittedChanges
-  
   validates_presence_of :domains
 end
